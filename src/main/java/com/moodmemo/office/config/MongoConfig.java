3b0253e5181afdebdc1251c574cf8f156fbd02a4
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-06-15 17:54:15 +0900

    feat: hide username & password (mongoDB)

--- src/main/java/com/moodmemo/office/config/MongoConfig.java
+++ src/main/java/com/moodmemo/office/config/MongoConfig.java
@@ -4,6 +4,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
@@ -14,6 +15,14 @@ import java.util.Collections;
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+    @Value("${spring.data.mongodb.server}")
+    private String server;
+
+
     @Override
     protected String getDatabaseName() {
         return "moodmemo";
@@ -21,7 +30,8 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/moodmemo");
+        ConnectionString connectionString = new ConnectionString("mongodb://"
+                + username + ":" + password + "@" + server + ":27017");
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
