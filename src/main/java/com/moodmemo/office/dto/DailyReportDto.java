b0522843401c611272b448d829cff56f4b72536d
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-06-16 09:24:46 +0900

    feat: create method find by kakaoId and between dateTime sort by dateTime

--- src/main/java/com/moodmemo/office/dto/DailyReportDto.java
+++ src/main/java/com/moodmemo/office/dto/DailyReportDto.java
@@ -1,5 +1,6 @@
 package com.moodmemo.office.dto;
 
+import com.moodmemo.office.domain.Stamps;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,14 +9,27 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class DailyReportDto {
-    private String kakaoId;
-    private String username;
-    private LocalDate date;
-    private String title, bodyText;
-    private List<String> keyword;
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Request {
+        private UserDto.Detail userDto;
+        private List<Stamps> todayStampList;
+
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response {
+        private String kakaoId;
+        private String username;
+        private LocalDate date;
+        private String title, bodyText;
+        private List<String> keyword;
+    }
 }
