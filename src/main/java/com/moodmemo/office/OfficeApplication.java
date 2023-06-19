e077ac4b3c7274bb62982cbd9c007230b2635b68
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-06-20 06:00:10 +0900

    hotfix: FE request
    FE 요청으로 cors 관련 코드 추가.
    이후에 FE 측에서 없어도 괜찮다고 하면 지울 예정.

--- src/main/java/com/moodmemo/office/OfficeApplication.java
+++ src/main/java/com/moodmemo/office/OfficeApplication.java
@@ -2,14 +2,33 @@ package com.moodmemo.office;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
 public class OfficeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OfficeApplication.class, args);
-		System.out.println("fight!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeApplication.class, args);
+        System.out.println("fight!");
+    }
+
+    @Bean // TODO - FE 에서 필요 없으면 삭제할 것
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+
 }
